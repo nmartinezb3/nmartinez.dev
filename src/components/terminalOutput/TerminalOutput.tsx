0432148dad8b05a900c4d6aa3d0cc849f97d9aea
Nicolas Martinez <nicolas.martinez@noblypos.com>
2020-06-09 21:57:54 -0300

    Added 'about' command

--- src/components/terminalOutput/TerminalOutput.tsx
+++ src/components/terminalOutput/TerminalOutput.tsx
@@ -5,12 +5,16 @@ import ExperienceCommandOutput from './ExperienceCommandOutput';
 
 enum Command {
   HELP = 'help',
+  ABOUT = 'about',
 }
 
 interface ITerminalOutputProps {
   command: string;
 }
 
+const Link = styled.a`
+  color: red;
+`;
 export function TerminalOutput(props: ITerminalOutputProps) {
   switch (props.command) {
     case Command.HELP:
@@ -19,6 +23,24 @@ export function TerminalOutput(props: ITerminalOutputProps) {
           Supported commands: <CommandText>about</CommandText>,{' '}
           <CommandText>experience [--all]</CommandText>, <CommandText>education</CommandText>,{' '}
           <CommandText>skills</CommandText>
+        </TerminalLineOutput>
+      );
+    case Command.ABOUT:
+      return (
+        <TerminalLineOutput>
+          Hi! <span>👋</span> <br />
+          I’m Nicolás Martínez, a 28 years-old Computer Science Engineer who graduated from{' '}
+          <Link href="https://www.fing.edu.uy/" target="_blank">
+            Facultad de Ingenieria of Universidad de la Republica, Uruguay
+          </Link>
+          . I am a senior full stack developer passionate about web and mobile application
+          development, especially using React, React Native, Node.js, and Mongo DB, although I'm
+          always open to learning new technologies. I'm currently working at{' '}
+          <Link href="https://www.noblypos.com/" target="_blank">
+            Nobly POS
+          </Link>{' '}
+          as a Full Stack Engineer. <br />
+          Outside of my job, I love surfing and traveling.
         </TerminalLineOutput>
       );
     default:
