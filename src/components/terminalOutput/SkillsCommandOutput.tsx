6a9554189ba7427782738bf9cb9950c165e0ab74
Nicolas Martinez <nicolas.martinez@noblypos.com>
2020-06-11 16:27:57 -0300

    Updated 'skills' command output

--- src/components/terminalOutput/SkillsCommandOutput.tsx
+++ src/components/terminalOutput/SkillsCommandOutput.tsx
@@ -1,55 +1,51 @@
 import React from 'react';
 import { TerminalLineOutput } from '../terminal/TerminalLine.styles';
-import { Link, CheckIconWrapper } from './TerminalOutput.styles';
+import { StarWrapper } from './TerminalOutput.styles';
 
-const CheckIcon = () => <CheckIconWrapper>✓</CheckIconWrapper>;
-// ★★★★☆☆☆☆
+interface IStarsProps {
+  rate: number;
+}
+
+const Stars: React.FunctionComponent<IStarsProps> = (props: IStarsProps) => {
+  return (
+    <>
+      {[...new Array(5)].map((value, index) => {
+        return <StarWrapper key={value}>{index < props.rate ? '★' : '☆'}</StarWrapper>;
+      })}
+    </>
+  );
+};
+
 const SkillsCommandOutput: React.FunctionComponent = () => {
   return (
     <TerminalLineOutput>
       <ul>
         <li>
-          JavaScript <CheckIcon />
-          <ul>
-            <li>
-              ES6 <CheckIcon />
-            </li>
-            <li>
-              TypeScript <CheckIcon />
-            </li>
-          </ul>
+          JavaScript <Stars rate={5} />
         </li>
         <li>
-          TypeScript <CheckIcon />
+          TypeScript <Stars rate={4} />
         </li>
         <li>
-          React <CheckIcon />
+          Node.js <Stars rate={5} />
         </li>
         <li>
-          React Native <CheckIcon />
+          React <Stars rate={5} />
         </li>
         <li>
-          HTML <CheckIcon />
+          React Native <Stars rate={4} />
         </li>
         <li>
-          CSS <CheckIcon />
+          HTML <Stars rate={4} />
         </li>
         <li>
-          Node <CheckIcon />
-          <ul>
-            <li>
-              Express.js <CheckIcon />
-            </li>
-            <li>
-              Koa.js <CheckIcon />
-            </li>
-            <li>
-              Mongoose <CheckIcon />
-            </li>
-          </ul>
+          CSS <Stars rate={4} />
         </li>
         <li>
-          Mongo DB <CheckIcon />
+          Mongo DB <Stars rate={5} />
+        </li>
+        <li>
+          PostgreSQL <Stars rate={4} />
         </li>
       </ul>
     </TerminalLineOutput>
--- src/components/terminalOutput/TerminalOutput.styles.ts
+++ src/components/terminalOutput/TerminalOutput.styles.ts
@@ -8,3 +8,6 @@ export const CheckIconWrapper = styled.span`
   color: #8dd39e;
   font-size: 1rem;
 `;
+export const StarWrapper = styled.span`
+  font-size: 20px;
+`;
